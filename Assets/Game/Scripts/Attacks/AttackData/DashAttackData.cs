7a8dabbd988af3e4f2236a715d4d6653d94de2f3
Tarpo <111potapovtaras111@gmail.com>
2023-04-27 16:34:18 +1000

    Dash

--- Assets/Game/Scripts/Attacks/AttackData/DashAttackData.cs
+++ Assets/Game/Scripts/Attacks/AttackData/DashAttackData.cs
@@ -5,6 +5,16 @@ public class DashAttackData : BaseAttackData
 {
     public float DashTime => _dashTime;
     public float DashSpeed => _dashSpeed;
+    public float DashDuration => _dashDuration;
+    public float DashDistance => _dashDistance;
+    public float YMultiply => _yMultiply;
+
+    public AnimationCurve YCurve => _yCurve;
+
     [SerializeField] private float _dashTime;
     [SerializeField] private float _dashSpeed;
+    [SerializeField] private float _dashDuration;
+    [SerializeField] private float _dashDistance;
+    [SerializeField] private float _yMultiply;
+    [SerializeField] private AnimationCurve _yCurve;
 }
--- Assets/Game/Scripts/Attacks/DashAttack.cs
+++ Assets/Game/Scripts/Attacks/DashAttack.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 [Serializable]
 public class DashAttack : BaseAttack
@@ -12,11 +13,18 @@ public class DashAttack : BaseAttack
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private AnimationComponent _animationComponent;
     private Collider[] _colliders = new Collider[5];
+    private float _startY;
 
     public override void StartAttack()
     {
-        // _animationComponent.PlayAnimation(_dashAttackData.AttackAnimation);
-        StartCoroutine(StartDash());
+        _navMeshAgent.enabled = false;
+        _startY = transform.position.y;
+        _animationComponent.PlayAnimation(_dashAttackData.AttackAnimation);
+        // StartCoroutine(StartDash());
+        Attacking = true;
+        StartCoroutine(LerpValue(transform.position,
+            transform.position + (transform.forward * _dashAttackData.DashDistance), _dashAttackData.DashDuration,
+            SetPosition, () => Attacking = false));
     }
 
     public override void StopAttack() => StopAllCoroutines();
@@ -48,6 +56,29 @@ public class DashAttack : BaseAttack
         }
 
         Attacking = false;
+    }
+
+    private IEnumerator LerpValue(Vector3 startValue, Vector3 endValue, float duration,
+        UnityAction<Vector3, float> action,
+        UnityAction onEnd = null)
+    {
+        float elapsed = 0;
+        while (elapsed <= duration)
+        {
+            action?.Invoke(Vector3.Lerp(startValue, endValue, elapsed / duration), elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        action?.Invoke(Vector3.Lerp(startValue, endValue, 1), 1);
+        _navMeshAgent.enabled = true;
+        onEnd?.Invoke();
+    }
+
+    private void SetPosition(Vector3 position, float timeValue)
+    {
+        transform.position =
+            position.WithY(_startY + _dashAttackData.YCurve.Evaluate(timeValue) * _dashAttackData.YMultiply);
     }
 
     private void Awake() => _dashAttackData.MeshParticlesPool.Load();
